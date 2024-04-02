381a5e4d6b94d1738315f80a3dbe5f23e7c45dd3
jiniyasshah <jiniyasshah@users.noreply.github.com>
2024-04-02 14:39:33 +0000

    fetch movies TMDB config custom hook searc developing

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Save() {
+    return (
+        <View
+            className="flex-1 justify-center items-center"
+        >
+            <Text className="text-5xl text-primary font-bold">Saved</Text>
+
+        </View>
+    );
+}
--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,4 @@
-import bg from "@/assets/images/bg.png";
+import bg from "@/assets/images/bg-1.png";
 import highlight from "@/assets/images/highlight.png";
 import rankingGradient from "@/assets/images/rankingGradient.png";
 
